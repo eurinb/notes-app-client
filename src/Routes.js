7b8467a864aa254dbf8064c7da11a3ee7e02ec8c
Ingo Brenckmann <Ingo.Brenckmann@gmail.com>
2019-04-01 22:25:00 +0200

    Note creation implemented

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login"
+import NewNote from "./containers/NewNote"
 import NotFound from "./containers/NotFound";
 
 export default ({childProps}) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     { /* Finally catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-  s3: {
-    REGION: "eu-west-1",
-    BUCKET: "notes-app-uploads-inb"
-  },
-  apiGateway: {
-    REGION: "eu-west-1",
-    URL: "https://dnz9ebq9dk.execute-api.eu-west-1.amazonaws.com/prod"
-  },
-  cognito: {
-    REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_7BLA1d5mv",
-    APP_CLIENT_ID: "5t47i8r1l39pnviejolvb01pc",
-    IDENTITY_POOL_ID: "eu-west-1:b53840ca-747f-4587-8dc1-298f255869b7"
-  }
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "notes-app-uploads-inb"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://dnz9ebq9dk.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_7BLA1d5mv",
+        APP_CLIENT_ID: "5t47i8r1l39pnviejolvb01pc",
+        IDENTITY_POOL_ID: "eu-west-1:b53840ca-747f-4587-8dc1-298f255869b7"
+    }
 };
